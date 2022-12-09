ab1ea1748019edac0b420bc3dca218243d3f8a1d
Romenildo Silva <romenildo2017@outlook.com>
2022-12-08 22:01:50 -0300

    desafio finalizado

---
+++ src/global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle, GlobalStyledComponent } from 'styled-components';
+
+
+export default createGlobalStyle`
+    *, body {
+        margin: 0;
+        padding: 0;
+    }
+`
